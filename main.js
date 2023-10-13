7953a934c17d3afb7be15f5146c59e5eb3f591f4
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-13 10:55:04 +0000

    last

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+alert('Skrypt działa prawidłowo!');
+// window.alert('Skrypt działa prawidłowo!');
 /* Opis
-
 1. Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
 2. Później w pliku main.js dodaj alert wyświetlający napis "Skrypt działa prawidłowo!"
 
