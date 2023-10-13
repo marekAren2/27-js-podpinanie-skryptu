67033cdeff4ef169e69d823babd5721ef031da8b
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-13 10:51:00 +0000

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Opis
+
+1. Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
+2. Później w pliku main.js dodaj alert wyświetlający napis "Skrypt działa prawidłowo!"
+
+Pliki wgraj do repozytorium zdalnego i podeślij link do sprawdzenia.
+ */
